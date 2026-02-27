ca544abb6fb3fb351506304ca8d9a751564338f4
CoolTL <thomasliisborg@gmail.com>
2026-02-27 13:30:55 +0100

    made some starting work on backend, now we kill ourselves

---
+++ sudoku.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Sudoku:
+    def __init__(self):
+        self.grid = [["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""],
+                     ["","","","","","","","",""]]
+    def rem_num(self, x, y):
+        self.grid[x][y] = ""
+
+    def change_num(self, x, y, n):
+        if n:
+            
+            if n == self.grid[x][y]:
+                self.rem_num(x, y)
+            else:
+                self.grid[x][y] = n
+        else:
+            self.rem_num(x,y)
+
+test = Sudoku()
+
+test.change_num(2,3,5)
+print(test.grid)
+print(type(test.grid[2][3]))
