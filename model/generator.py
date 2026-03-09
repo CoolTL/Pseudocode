90f1c004dd344655b502f3c6f7e127078d4b2ed4
NEXT15930 <fake@email.com>
2026-03-09 11:07:03 +0100

    Update gitignore, lookup table generator complete

---
+++ model/generator.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from seeds import *
+from random import randrange
+import numpy as np
+
+class Generator(ABC):
+    """ Abstract base class for sudoku generators. """
+
+    def __init__(self):
+        self.seed = EASY_SEED_SOLVED
+        self.convert_to_numbers(self.seed)
+        
+    # @abstractmethod
+    def generate_lookup_table(self):
+        """ Randomly picks which number is correspondent to which letter """
+        nums = np.linspace(start=1, stop=9, num=9, dtype=int)
+        letters = "abcdefghi"
+        lookup = []
+        for i in letters:
+            choice = randrange(0, len(nums))
+            lookup.append(int(nums[choice]))
+            nums = np.delete(nums, choice)
+        return lookup
+
+    def convert_to_numbers(self, seed):
+        """ Converts the list of letters into numbers """
+        lookup = self.generate_lookup_table()
+        sudoku = 
+
+    def turn_into_sudoku(self):
+        """ This function converts the list of numbers back into the backend format we use """
+        pass
+
+# https://gamedev.stackexchange.com/questions/56149/how-can-i-generate-sudoku-puzzles
+# Numpy methods I can use
+# .rot90 to rotate the sudoku seed
+# .flip/flipud/fliplr to flip the sudoku seed
+test = Generator()
+test.generate_lookup_table()
---
+++ model/seeds.py
@@ -0,0 +1,2 @@
+EASY_SEED = "hfi--cge---cgaif--a---hf--d---acge-fdg--e--a-ca----ig--eh---dc----b-e--g-iac-----"
+EASY_SEED_SOLVED = "hfidbcgeaedcgaifbhabgehfcidihbacgedfdgfiebhaccaehfdigbbehfgadcifcdbieahggiacdhbfe"
