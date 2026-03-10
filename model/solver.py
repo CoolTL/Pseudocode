1adb81dcb6c78c4075ec9cbcdb20753da6957e49
CoolTL <thomasliisborg@gmail.com>
2026-03-10 12:06:09 +0100

    done some work on solver, the shit is so fucking fucked

--- model/solver.py
+++ model/solver.py
@@ -1,7 +1,30 @@
 from sudoku import Sudoku
+import numpy as np
 class solver():
+    
     def __init__(self, sudokugrid):
+
         self.sudoku = self.sudokugrid
+
+    def box_check(n, m, grid):
+        box_num = []
+        # Checking each value in a box
+        for l in range((n-3), n):
+            for p in range((m-3), m):
+            # Checking if the number has already appeared or not, if not then it adds it to numbers seen
+                if grid[p][l] not in box_num:
+                    box_num.append(grid[p][l])
+        return box_num 
+    
+    def col_check(grid, k):
+        # Creating a list to keep track of numbers in colloumns
+        col_num = []
+        # Checking each value in a colloumn
+        for r in range(9):
+            # Checks if the number has alreadey appeared or not, if not then it adds it to numbers seen
+            if grid[r][k] not in col_num:
+                col_num.append(grid[r][k])
+        return col_num
 
     def check(grid):
         # Setting up variables
@@ -24,33 +47,34 @@ class solver():
                 cor_rows.append(row_num)
         # Checking each colloumn
         for k in range(9):
-            # Creating a list to keep track of numbers in colloumns
-            col_num = []
-            # Checking each value in a colloumn
-            for r in range(9):
-                # Checks if the number has alreadey appeared or not, if not then it adds it to numbers seen
-                if grid[r][k] not in col_num:
-                    col_num.append(grid[r][k])
+            col = col_check(grid, k)
             # Checks if there are nine numbers, if yes then adds it to correct colloumns
             if len(col_num) == 9:
                 cor_col.append(col_num)
         # Checking each box
         for o in range(9):
-            # Creating a list to keep track of numbers in a box
-            box_num = []
-            # Checking each value in a box
-            for l in range((n-3), n):
-                for p in range((m-3), m):
-                    # Checking if the number has already appeared or not, if not then it adds it to numbers seen
-                    if grid[p][l] not in box_num:
-                        box_num.append(grid[p][l])
-                # Checks if there are nine numbers, if yes adds it to correct colloumns
-                if len(box_num) == 9:
-                    cor_box.append(box_num)
-                    # Checks
-                    if m == 9:
-                        m = 3
-                        n += 3
+            box = check_box(n, m, grid)
+           # Checks if there are nine numbers, if yes adds it to correct colloumns
+            if len(box) == 9:
+                cor_box.append(box)
+                # Box variables 
+                if m == 9:
+                    m = 3
+                    n += 3
+                else:
+                    m += 3
+        if len(cor_box) == 9 and len(cor_rows) == 9 and len(cor_col) == 9:
+            return True
+
+    def solve(sudoku):
+        cor_nums = {1,2,3,4,5,6,7,8,9}
+
+        while True:
+            for i in sudoku:
+                for j in i:
+                    if j != "":
+                        pass
                     else:
-                        m += 3
+
                     
+                    self.sudoku.change_num
