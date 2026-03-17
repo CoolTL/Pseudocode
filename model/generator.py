1f1c542a517b26d005ba3021abeb2dd866197837
NEXT15930 <fake@email.com>
2026-03-17 10:47:56 +0100

    Clarify code, switch to storing seeds with numbers and added more

--- model/generator.py
+++ model/generator.py
@@ -8,9 +8,15 @@ class Generator(ABC):
 
     def __init__(self):
         seed = EASY_SEED_SOLVED
-        self.turn_into_sudoku(self.convert_to_numbers(seed))
+        self.convert_to_matrix(self.convert_to_numbers(self.prepare_seed(HARD_SEED_SOLVED)))
         
-    # @abstractmethod
+    def prepare_seed(self, seed):
+        """ Turns the seed into the letters a-i so it the numbers can be scrambled """
+        letters = "abcdefghi"
+        for i in range(0, len(letters)):
+            seed = seed.replace(str(i+1), letters[i])
+        return seed
+    
     def generate_lookup_table(self):
         """ Randomly picks which number is correspondent to which letter """
         nums = np.linspace(start=1, stop=9, num=9, dtype=int)
@@ -25,24 +31,23 @@ class Generator(ABC):
         """ Converts the list of letters into numbers """
         lookup = self.generate_lookup_table()
         letters = "abcdefghi"
-        sudoku = seed
         for i in range(0, len(letters)):
-            sudoku = sudoku.replace(letters[i], str(lookup[i]))            
-        return sudoku
-
-    def rotate_seed(self, seed):
-        """ Rotate the seed randomly between its 4 sides """
-        return np.rot90(seed, randrange(0, 4), axes=(0,1))
-
-    def flip_seed(self, seed):
-        """ Flip the seed either horizontally, vertically or not at all """
-        if randrange(0, 2) == 1:
-            seed = np.flipud(seed)
-        if randrange(0, 2) == 1:
-            seed = np.fliplr(seed)
+            seed = seed.replace(letters[i], str(lookup[i]))            
         return seed
 
-    def turn_into_sudoku(self, seed):
+    def rotate_matrix(self, matrix):
+        """ Rotate the matrix randomly between its 4 sides """
+        return np.rot90(matrix, randrange(0, 4), axes=(0,1))
+
+    def flip_matrix(self, matrix):
+        """ Flip the matrix either horizontally, vertically or not at all """
+        if randrange(0, 2) == 1:
+            matrix = np.flipud(matrix)
+        if randrange(0, 2) == 1:
+            matrix = np.fliplr(matrix)
+        return matrix
+
+    def convert_to_matrix(self, seed):
         """ This function converts the list of numbers back into the backend format we use """
         sudoku = np.empty((9, 9), dtype=int)
         counter = 0
@@ -51,8 +56,8 @@ class Generator(ABC):
                 sudoku[i][j] = seed[counter]
                 counter += 1
         # Flip or rotate the sudokus for more randomness
-        sudoku = self.flip_seed(sudoku)
-        sudoku = self.rotate_seed(sudoku)
+        sudoku = self.flip_matrix(sudoku)
+        sudoku = self.rotate_matrix(sudoku)
         print(sudoku)
 
 # https://gamedev.stackexchange.com/questions/56149/how-can-i-generate-sudoku-puzzles
