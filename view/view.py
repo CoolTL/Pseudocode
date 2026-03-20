239a0deb1de430abb51c8562cce8009be4ae967a
NEXT15930 <fake@email.com>
2026-03-20 13:07:22 +0100

    More view code (not working)

--- view/view.py
+++ view/view.py
@@ -11,8 +11,23 @@ class View(QObject):
         super().__init__()
         self.ui = loader.load("game.ui", None)
 
+        # Create list of all cell buttons
+        self.cells = []
+        for i in range(0, 9):
+            for j in range(0, 9):
+                # This doesn't work
+                self.cells.append(f"self.ui.s{i}{j}")
+        print(self.cells)
+
+        # Connect all buttons
+        for cell in self.cells:
+            cell.clicked.connect( lambda checked, cell=cell : self.pressed(cell))
+        
         # This variable stores which button the user has selected
         self.selected_cell = None
+
+    def pressed(self, cell):
+        pass
 
     def cell_selected(self, cell):
         """ Change which cell is selected """
