f76755a7ec749a5c83a0c633dc6b1e4aa7324381
CoolTL <thomasliisborg@gmail.com>
2026-04-24 13:15:32 +0200

    fixed up solver a bit

--- model/solver.py
+++ model/solver.py
@@ -4,29 +4,29 @@ class solver():
     
     def __init__(self, sudokugrid):
 
-        self.sudoku = self.sudokugrid
+        self.sudoku = sudokugrid
 
-    def box_check(n, m, grid):
-        box_num = {}
+    def box_check(self, n, m, grid):
+        box_num = set()
         # Checking each value in a box
         for l in range((n-3), n):
             for p in range((m-3), m):
             # Checking if the number has already appeared or not, if not then it adds it to numbers seen
                 if grid[p][l] not in box_num:
-                    box_num.append(grid[p][l])
+                    box_num.add(grid[p][l])
         return box_num 
     
-    def col_check(grid, k):
+    def col_check(self, grid, k):
         # Creating a list to keep track of numbers in colloumns
-        col_num = {}
+        col_num = set()
         # Checking each value in a colloumn
         for r in range(9):
             # Checks if the number has alreadey appeared or not, if not then it adds it to numbers seen
             if grid[r][k] not in col_num:
-                col_num.append(grid[r][k])
+                col_num.add(grid[r][k])
         return col_num
 
-    def check(grid):
+    def check(self, grid):
         # Setting up variables
         cor_rows = []
         cor_col = []
@@ -47,13 +47,13 @@ class solver():
                 cor_rows.append(row_num)
         # Checking each colloumn
         for k in range(9):
-            col = col_check(grid, k)
+            col = self.col_check(grid, k)
             # Checks if there are nine numbers, if yes then adds it to correct colloumns
-            if len(col_num) == 9:
-                cor_col.append(col_num)
+            if len(col) == 9:
+                cor_col.append(col)
         # Checking each box
         for o in range(9):
-            box = check_box(n, m, grid)
+            box = self.box_check(n, m, grid)
            # Checks if there are nine numbers, if yes adds it to correct colloumns
             if len(box) == 9:
                 cor_box.append(box)
@@ -72,33 +72,31 @@ class solver():
 
         while True:
             for i in sudoku:
-                for j in len(i):
+                for j in range(len(i)):
                     if i[j] != "":
                         pass
                     else:
                         counter += 1
-                        if soduku.index(i) => 7:
+                        if sudoku.index(i) >= 6:
                             a = 9
-                        elif sudoku.index(i) => 4:
+                        elif sudoku.index(i) >= 3:
                             a = 6
                         else:
                             a = 3
                         
-                        if i[j] => 7:
+                        if j >= 6:
                             b = 9
-                        elif i[j] => 4:
+                        elif j >= 3:
                             b = 6
                         else:
                             b = 3
 
-                        m = col_check(sudoku, j) 
-                        l = box_check(a, b, sudoku)
-                        n = cor_nums-i
-                        n = n-m
-                        n = n-l
+                        m = self.col_check(sudoku, j) 
+                        l = self.box_check(a, b, sudoku)
+                        n = cor_nums-set(i) - set(m) - set(l)
 
                         if len(n) == 1:
-                            k = n[0]
+                            k = list(n)[0]
                             self.sudoku.change_num(j, k)
                             j = k
                             counter -= 1
